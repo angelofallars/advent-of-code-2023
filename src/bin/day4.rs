6c279a2850358b6b6d22d061502802d49205e887
Angelo Fallaria <ba.fallaria@gmail.com>
2023-12-05 02:48:34 +0800

    refactor(d4): put core funcs inside external funcs

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -42,49 +42,51 @@ mod lexer {
     }
 
     pub fn lex(input: Vec<char>) -> Vec<Token> {
-        lex_core(&input, 0, Vec::new())
-    }
-    fn lex_core(input: &Vec<char>, pos: usize, tokens: Vec<Token>) -> Vec<Token> {
-        if is_end(input, pos) {
-            return tokens;
-        }
+        return aux(&input, 0, Vec::new());
 
-        let pos = skip_whitespace(input, pos);
-
-        use Token::*;
-        let c = input[pos];
-        let (pos, token) = match c {
-            '0'..='9' => {
-                let (pos, number) = read_number(input, pos);
-                (pos, Number(number))
+        fn aux(input: &Vec<char>, pos: usize, tokens: Vec<Token>) -> Vec<Token> {
+            if is_end(input, pos) {
+                return tokens;
             }
-            'C' => {
-                let (pos, ident) = read_ident(input, pos);
-                if ident != "Card" {
-                    panic!("Invalid identifier at {}: {}", pos, ident);
+
+            let pos = skip_whitespace(input, pos);
+
+            use Token::*;
+            let c = input[pos];
+            let (pos, token) = match c {
+                '0'..='9' => {
+                    let (pos, number) = read_number(input, pos);
+                    (pos, Number(number))
                 }
-                (pos, Card)
-            }
-            ':' => (advance(pos), Colon),
-            '|' => (advance(pos), Pipe),
-            '\n' => (advance(pos), Newline),
-            _ => {
-                panic!("Unknown character at {}: {}", pos, c)
-            }
-        };
+                'C' => {
+                    let (pos, ident) = read_ident(input, pos);
+                    if ident != "Card" {
+                        panic!("Invalid identifier at {}: {}", pos, ident);
+                    }
+                    (pos, Card)
+                }
+                ':' => (advance(pos), Colon),
+                '|' => (advance(pos), Pipe),
+                '\n' => (advance(pos), Newline),
+                _ => {
+                    panic!("Unknown character at {}: {}", pos, c)
+                }
+            };
 
-        lex_core(input, pos, append(tokens, token))
+            aux(input, pos, append(tokens, token))
+        }
     }
 
     fn skip_whitespace(input: &Vec<char>, pos: usize) -> usize {
-        skip_whitespace_core(input, pos)
-    }
-    fn skip_whitespace_core(input: &Vec<char>, pos: usize) -> usize {
-        let c = input[pos];
-        if c != ' ' {
-            return pos;
+        return aux(input, pos);
+
+        fn aux(input: &Vec<char>, pos: usize) -> usize {
+            let c = input[pos];
+            if c != ' ' {
+                return pos;
+            }
+            aux(input, advance(pos))
         }
-        skip_whitespace_core(input, advance(pos))
     }
 
     fn read_number(input: &Vec<char>, pos: usize) -> (usize, usize) {
@@ -111,20 +113,21 @@ mod lexer {
         predicate: fn(char) -> bool,
         map: fn(Vec<char>) -> T,
     ) -> (usize, T) {
-        read_sequence_core(input, pos, predicate, map, Vec::new())
-    }
-    fn read_sequence_core<T>(
-        input: &Vec<char>,
-        pos: usize,
-        predicate: fn(char) -> bool,
-        map: fn(Vec<char>) -> T,
-        chars: Vec<char>,
-    ) -> (usize, T) {
-        let c = input[pos];
-        if is_end(input, pos) || !predicate(c) {
-            return (pos, map(chars));
+        return aux(input, pos, predicate, map, Vec::new());
+
+        fn aux<T>(
+            input: &Vec<char>,
+            pos: usize,
+            predicate: fn(char) -> bool,
+            map: fn(Vec<char>) -> T,
+            chars: Vec<char>,
+        ) -> (usize, T) {
+            let c = input[pos];
+            if is_end(input, pos) || !predicate(c) {
+                return (pos, map(chars));
+            }
+            aux(input, advance(pos), predicate, map, append(chars, c))
         }
-        read_sequence_core(input, advance(pos), predicate, map, append(chars, c))
     }
 }
 
@@ -139,28 +142,29 @@ mod parser {
     }
 
     pub fn parse(tokens: Vec<Token>) -> Vec<Card> {
-        parse_core(&tokens, 0, Vec::new())
-    }
-    fn parse_core(tokens: &Vec<Token>, pos: usize, cards: Vec<Card>) -> Vec<Card> {
-        if is_end(tokens, pos) {
-            return cards;
+        return aux(&tokens, 0, Vec::new());
+
+        fn aux(tokens: &Vec<Token>, pos: usize, cards: Vec<Card>) -> Vec<Card> {
+            if is_end(tokens, pos) {
+                return cards;
+            }
+
+            let token = &tokens[pos];
+            use Token::*;
+            let (pos, card) = match token {
+                Card => parse_card(tokens, pos),
+                _ => panic!("Invalid token at {}: {:?}", pos, token),
+            };
+
+            aux(tokens, pos, append(cards, card))
         }
-
-        let token = &tokens[pos];
-        use Token::*;
-        let (pos, card) = match token {
-            Card => parse_card(tokens, pos),
-            _ => panic!("Invalid token at {}: {:?}", pos, token),
-        };
-
-        parse_core(tokens, pos, append(cards, card))
     }
 
     fn parse_card(tokens: &Vec<Token>, pos: usize) -> (usize, Card) {
         let pos = advance(pos);
         expect_token(Token::Number(0), &tokens[pos]);
         let Token::Number(id) = tokens[pos]
-    else { unreachable!() };
+        else { unreachable!() };
 
         let pos = advance(pos);
         expect_token(Token::Colon, &tokens[pos]);
@@ -173,29 +177,38 @@ mod parser {
     }
 
     fn parse_set_a(tokens: &Vec<Token>, pos: usize) -> (usize, Vec<usize>) {
-        parse_set_core(tokens, pos, mem::discriminant(&Token::Pipe), Vec::new())
+        parse_set(tokens, pos, mem::discriminant(&Token::Pipe))
     }
     fn parse_set_b(tokens: &Vec<Token>, pos: usize) -> (usize, Vec<usize>) {
-        parse_set_core(tokens, pos, mem::discriminant(&Token::Newline), Vec::new())
+        parse_set(tokens, pos, mem::discriminant(&Token::Newline))
     }
-    fn parse_set_core(
+
+    fn parse_set(
         tokens: &Vec<Token>,
         pos: usize,
         end_token: mem::Discriminant<Token>,
-        numbers: Vec<usize>,
     ) -> (usize, Vec<usize>) {
-        if is_end(tokens, pos) {
-            return (pos, numbers);
-        }
-        if end_token == mem::discriminant(&tokens[pos]) {
-            return (advance(pos), numbers);
-        }
+        return aux(tokens, pos, end_token, Vec::new());
 
-        expect_token(Token::Number(0), &tokens[pos]);
-        let Token::Number(number) = tokens[pos]
-    else { unreachable!() };
+        fn aux(
+            tokens: &Vec<Token>,
+            pos: usize,
+            end_token: mem::Discriminant<Token>,
+            numbers: Vec<usize>,
+        ) -> (usize, Vec<usize>) {
+            if is_end(tokens, pos) {
+                return (pos, numbers);
+            }
+            if end_token == mem::discriminant(&tokens[pos]) {
+                return (advance(pos), numbers);
+            }
 
-        parse_set_core(tokens, advance(pos), end_token, append(numbers, number))
+            expect_token(Token::Number(0), &tokens[pos]);
+            let Token::Number(number) = tokens[pos]
+            else { unreachable!() };
+
+            aux(tokens, advance(pos), end_token, append(numbers, number))
+        }
     }
 
     fn expect_token(expected_token: Token, token: &Token) {
@@ -209,15 +222,16 @@ mod evaluator {
     use super::*;
 
     pub fn eval_pt1(cards: &Vec<Card>) -> usize {
-        eval_pt1_core(cards, 0)
-    }
-    fn eval_pt1_core(cards: &Vec<Card>, pos: usize) -> usize {
-        if is_end(cards, pos) {
-            return 0;
-        }
+        return aux(cards, 0);
 
-        let card = &cards[pos];
-        calculate_points(card) + eval_pt1_core(cards, advance(pos))
+        fn aux(cards: &Vec<Card>, pos: usize) -> usize {
+            if is_end(cards, pos) {
+                return 0;
+            }
+
+            let card = &cards[pos];
+            calculate_points(card) + aux(cards, advance(pos))
+        }
     }
 
     fn calculate_points(card: &Card) -> usize {
@@ -241,35 +255,32 @@ mod evaluator {
             .map(|card| calculate_matches(card))
             .collect();
         let instances: Vec<usize> = cards.into_iter().map(|_| 1).collect();
-        eval_pt2_core(cards, points, 0, instances)
-    }
-    fn eval_pt2_core(
-        cards: &Vec<Card>,
-        points: Vec<usize>,
-        pos: usize,
-        instances: Vec<usize>,
-    ) -> usize {
-        if is_end(cards, pos) {
-            return instances.into_iter().sum();
+
+        return aux(cards, points, 0, instances);
+
+        fn aux(cards: &Vec<Card>, points: Vec<usize>, pos: usize, instances: Vec<usize>) -> usize {
+            if is_end(cards, pos) {
+                return instances.into_iter().sum();
+            }
+
+            let multiplier = instances[pos];
+            let copy_ids: Vec<usize> = (0..points[pos])
+                .into_iter()
+                .map(|point| point + pos + 1)
+                .collect();
+            let instances = instances
+                .into_iter()
+                .enumerate()
+                .map(|(idx, instance)| {
+                    if copy_ids.contains(&idx) {
+                        instance + multiplier
+                    } else {
+                        instance
+                    }
+                })
+                .collect();
+
+            aux(cards, points, advance(pos), instances)
         }
-
-        let multiplier = instances[pos];
-        let copy_ids: Vec<usize> = (0..points[pos])
-            .into_iter()
-            .map(|point| point + pos + 1)
-            .collect();
-        let instances = instances
-            .into_iter()
-            .enumerate()
-            .map(|(idx, instance)| {
-                if copy_ids.contains(&idx) {
-                    instance + multiplier
-                } else {
-                    instance
-                }
-            })
-            .collect();
-
-        eval_pt2_core(cards, points, advance(pos), instances)
     }
 }
